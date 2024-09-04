bc203a39684bc0015d2c460af932aadff3349f6c
Tomas David Ye <tomas@tomasdavidye.tech>
2024-09-04 12:53:08 +0200

    [ADDING DATA LOADING]

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
-# This is a sample Python script.
+from backend.src.db.DBContext import DBContext
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+def load_trend_data(name):
+
+    # Reach out to me at tom@perseuss.xyz if you want access to the full trend database
+    db = DBContext.getTrendDatabaseSql()
+
+    raw_terms_df = db.load_select_query_to_df('''
+    select t.*
+    from weekly_time_range_stats_raw as s
+        join terms_time_series_weekly as t
+             on s.term = t.term
+    where s.start_date = '2024-04-13'
+      and s.end_date = '2024-08-31'
+      and s.volume_growth_percent > 100
+      and s.min_volume > 100
+    order by term asc, date asc
+    ''')
+
+    raw_terms_df.to_csv('./raw_term_sample.txt', index=False)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
 
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    load_trend_data('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
